4f487a12a7cb57699b959a026163e6c2e07cde75
divpreeet <ytbraced@gmail.com>
2026-04-22 13:54:13 +0400

    next version for main fix

--- Formula/whereisthis.rb
+++ Formula/whereisthis.rb
@@ -3,8 +3,8 @@ class Whereisthis < Formula
 
   desc "Human language based file finder"
   homepage "https://github.com/divpreeet/whereisthis"
-  url "https://github.com/divpreeet/whereisthis/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "d40f0dcafeb32d5733e9c29245e15102fa8c57f4af1b5e1601f855e6664d41e3"
+  url "https://github.com/divpreeet/whereisthis/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "e78145df5ea2700cb40cc0bf09209225764c4544d6ce17a23a9708bbadb2c2e7"
   license "MIT"
 
   depends_on "python@3.12"
